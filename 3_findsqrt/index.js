d1641fb056c0912f3497a78a17c44284d1b0758f
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-24 09:19:25 -0500

    Se arregló el cod de Ej 3

--- 3_findsqrt/index.js
+++ 3_findsqrt/index.js
@@ -1,11 +1,12 @@
 const btn = document.querySelector(".btn")
 const answer = document.querySelector(".answer")
+const number = document.querySelector(".number")
 
 btn.addEventListener("click",stdnNumber)
 
 function stdnNumber(){
-    let a = prompt("Pon numero a sacar raiz cuadrada")
-
+    let a = number.value;
+    console.log(a);
     if (a < 0) { 
         alert("Los numeros negativos no tienen raiz cuadrada, es imaginaria")
         alert("Vuelva a oprimir botón y seleccione un númer positivo")
