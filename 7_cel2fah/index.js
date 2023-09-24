5207d1cb716b16818c47fb34bf0a9991a22d668a
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-24 15:35:16 -0500

    Se realizo ejercicio convertir celcius a Fahrenheit

---
+++ 7_cel2fah/index.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn");
+const cInput = document.querySelector(".cInput");
+//Cuadro de respuesta
+const answ = document.querySelector(".answ");
+
+
+btn.addEventListener("click", c2f);
+
+function c2f() {
+  let c = cInput.value;
+  let f= (c*9/5)+32;
+  console.log(`°${c} Celcius equivale a: ${f} Fahrenheit`);
+  answ.innerText = `${c}° Celcius equivale a: ${f} Fahrenheit`;
+  
+  /* Para hacerlo sin variable temporal
+  a=a+b;
+  b=a-b
+  a=a-b  
+  */
+
+
+}
