05d25ede180fbcb0567d921b967611d706023316
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-25 17:35:31 -0500

    Se completó js para hacer factorial con una funcion recursiva

--- 13_findFactNum/index.js
+++ 13_findFactNum/index.js
@@ -6,13 +6,24 @@ const answ = document.querySelector(".answ");
 btn.addEventListener("click", facNum);
 
 function facNum() {
-  let numFac = num.value;
-  var res=1
-  console.log(numFac);
+  let numFac = parseInt(num.value);
+  var res = 1;
+  // console.log(numFac);
   for (let i = 1; i <= numFac; i++) {
-    var multi = res*i // 1, 2, 6, 12
-    res = multi
+    var multi = res * i; // 1, 2, 6, 12
+    res = multi;
   }
+  let facRecursive=facNumRecursion(num.value); //Llama a funcion recursiva
+  console.log(facRecursive); // Log recursivo
   console.log(multi);
   answ.innerText = `El numero ${num.value} tiene de facotial= ${multi}`;
+}
+
+function facNumRecursion(n) {
+  // let n=num.value;
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * facNumRecursion(n - 1); // Recursion, llama a la misma funcion para terminar proceso
+  }
 }
