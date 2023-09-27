006598cdc5c69cb68be9c6acd678201c8e62bc71
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-27 16:14:17 -0500

    Se modifica la letra de cada texto a mayuscula

--- 20_firsLettUp/index.js
+++ 20_firsLettUp/index.js
@@ -10,10 +10,14 @@ function firstLetUp() {
   const word = wordA.value.toLowerCase(); //Convertir text a lowerCase
   const wordSplit = word.split("");
 
-  var mayus = wordSplit[0].toUpperCase();
-
+  var mayus = wordSplit[0].toUpperCase(); //Propio
   for (let i = 1; i < word.length; i++) {
     mayus += wordSplit[i];
   }
   console.log(mayus);
+
+  let mayusB = word.charAt(0).toUpperCase(); //Usando charAt
+  let rest = wordSplit.slice(1, word.length).join("");
+  let result = mayusB + rest;
+  console.log(result);
 }
